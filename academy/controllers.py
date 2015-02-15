774a5a511f6c9ca1283254af7b630311d07cba71
Juan Fernando Jaramillo Botero <juanf.jaramillo@opdevel.com>
2015-02-14 20:28:57 -0500

    URLs and routing 1

--- academy/controllers.py
+++ academy/controllers.py
@@ -2,22 +2,13 @@
 from openerp import http
 
 class Academy(http.Controller):
-     @http.route('/academy/academy/', auth='public', website=True)
-     def index(self, **kw):
-         Teachers = http.request.env['academy.teachers']
-         return http.request.render('academy.index', {
-             'teachers': Teachers.search([])
-         })
+    @http.route('/academy/academy/', auth='public', website=True)
+    def index(self, **kw):
+        Teachers = http.request.env['academy.teachers']
+        return http.request.render('academy.index', {
+            'teachers': Teachers.search([])
+        })
 
-#     @http.route('/academy/academy/objects/', auth='public')
-#     def list(self, **kw):
-#         return http.request.render('academy.listing', {
-#             'root': '/academy/academy',
-#             'objects': http.request.env['academy.academy'].search([]),
-#         })
-
-#     @http.route('/academy/academy/objects/<model("academy.academy"):obj>/', auth='public')
-#     def object(self, obj, **kw):
-#         return http.request.render('academy.object', {
-#             'object': obj
-#         })
+    @http.route('/academy/<name>/', auth='public', website=True)
+    def teacher(self, name):
+        return '<h1>{}</h1>'.format(name)
