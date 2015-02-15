6a216a1ccbd9104f785459d3c2f5d32374c180a6
Juan Fernando Jaramillo Botero <juanf.jaramillo@opdevel.com>
2015-02-15 08:54:25 -0500

    Relations between models 1

--- academy/models.py
+++ academy/models.py
@@ -7,3 +7,9 @@ class Teachers(models.Model):
 
     name = fields.Char()
     biography = fields.Html()
+
+class Courses(models.Model):
+    _name = 'academy.courses'
+
+    name = fields.Char()
+    teacher_id = fields.Many2one('academy.teachers', string="Teacher")
